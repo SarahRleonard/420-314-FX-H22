5fd9153e59dc152b3ae4e0681cd874597801e590
Emy <e.leclere@edu.cegepgarneau.ca>
2022-01-04 14:47:57 -0500

    composante bouton

--- lenkwai_nabilbouazzaoui/src/lib/Bouton/Bouton.js
+++ lenkwai_nabilbouazzaoui/src/lib/Bouton/Bouton.js
@@ -1,3 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import useLogic from './useLogic';
+import React from "react";
+import {Button} from 'antd';
+import {PropTypes} from 'prop-types';
+
+export function Bouton({backgroundColor, size, ...props}) {
+    return (
+        <Button>
+          Exemple
+        </Button>
+      );
+    };
+
+Bouton.propTypes = {
+
+    backgroundColor: PropTypes.string,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+};
+
+Bouton.defaultProps = {
+    backgroundColor: null,
+    size: 'medium',
+  };
+  
+
+export default Bouton;
