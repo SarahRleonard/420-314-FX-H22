8b616f97cdf738a5ce8531a49785124ff6a2fefd
HBherer[Desktop] <77289920+HBherer@users.noreply.github.com>
2022-01-06 11:23:26 -0500

    Add 2 components (DropDown, Time) and dependencies

--- zwgagne_hbherer/src/environment/App/App.js
+++ zwgagne_hbherer/src/environment/App/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Counter} from 'lib';
+import Time from '../../lib/Time/Time';
+import Counter from '../../lib/Counter/Counter';
+import DropDown from '../../lib/DropDown/DropDown';
 import classes from './App.module.css';
 
 const App = () => (
-  <div className={classes.container}>
-    <Counter initialValue={0} />
-  </div>
+  <>
+    <div className={classes.container}>
+      <Counter initialValue={0} />
+    </div>
+    <div className={classes.container}>
+      <DropDown 
+      Name1='Mon profil' Link1='/profil'
+      Name2='Accueil' Link2='/'
+      Name3='Mon inventaire' Link3='/inventory'
+      Name4='Favories' Link4='/fav'
+      />
+    </div>
+    <div className={classes.containerLeft}>
+      <Time />
+    </div>
+  </>
 );
 
 export default App;
