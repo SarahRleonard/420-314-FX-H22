c5cb4ee5ca361d8bff98c0b24dda1cb12105b7b5
NabilBouazzaoui <73765566+NabilBouazzaoui@users.noreply.github.com>
2022-01-08 14:39:27 -0500

    ajout du fichier dans lib

---
+++ lenkwai_nabilbouazzaoui/src/lib/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Breadcrumb} from 'antd';
+import {HomeOutlined, QqOutlined} from '@ant-design/icons';
+import './bread.css';
+
+export function Breadcrumbs(...props){
+    return(
+        <Breadcrumb>
+        <Breadcrumb.Item><a href="">Accueil</a></Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <a href="">Lorem</a>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>Active</Breadcrumb.Item>
+      </Breadcrumb>
+    );
+};
+
+export function BreadcrumbIcons(...props){
+    return(
+        <Breadcrumb>
+    <Breadcrumb.Item href="">
+      <HomeOutlined />
+    </Breadcrumb.Item>
+    <Breadcrumb.Item href="">
+      <QqOutlined />
+      <span>Lorem</span>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>Active</Breadcrumb.Item>
+  </Breadcrumb>
+    );
+};
+export default Breadcrumbs;
