61cc73bc8162002211e738d37a6373c0c0ca473b
sam22may <74010119+sam22may@users.noreply.github.com>
2022-01-09 14:09:22 -0500

    remove test de style du checkbox

--- slemay_ntremblay/src/lib/CheckboxUi/CheckboxUi.js
+++ slemay_ntremblay/src/lib/CheckboxUi/CheckboxUi.js
@@ -3,13 +3,12 @@ import {Checkbox} from 'antd';
 import PropTypes from 'prop-types';
 import styles from './Checkbox.module.css';
 
-const CheckboxUi = ({text, textColor}) => (
-  <Checkbox className={textColor}>{text}</Checkbox>
+const CheckboxUi = ({text}) => (
+  <Checkbox>{text}</Checkbox>
 );
 
 CheckboxUi.propTypes = {
   text: PropTypes.string.isRequired,
-  textColor: PropTypes.string.isRequired,
 };
 
 export default CheckboxUi;
