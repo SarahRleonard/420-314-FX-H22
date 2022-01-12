e65455ce188ab2437e04d634d34f42d90fdc0ca8
hervegondie <gondie.herve@gmail.com>
2022-01-11 23:52:09 -0500

    composants bouton.composants.composants.

---
+++ zwgagne_hbherer/src/lib/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <div>
+         <input className="favorite styled"
+       type="button"
+       value="Add to favorites"/>
+        </div>
+    )
+}
+
+export default Button
